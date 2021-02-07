1d141d4e1298fc664722481359b2be8cb04de5d2
Sarthak <pusarthak@gmail.com>
2021-02-07 16:49:05 -0600

    rotate function fixed errors

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -1,6 +1,6 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
-using namespace cs225;  
+using namespace cs225; 
 
 #include <string>
 
@@ -8,24 +8,26 @@ using namespace cs225;
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 
 
-  PNG * ourPNG = new PNG();
-  ourPNG->readFromFile(inputFile);
+  PNG * ourPNG = nullptr;
+  ourPNG->readFromFile(inputFile); 
 
-  PNG * newPNG = new PNG(ourPNG->width(), ourPNG->height());
+  PNG newPNG(ourPNG->width(), ourPNG->height()); 
 
-  for (unsigned int x = 0; x < ourPNG->width(); x++) {
-    for (unsigned int y = 0; y < ourPNG->height(); y++) {
+for (unsigned int x = 0; x < ourPNG->width(); x++) { 
+  for (unsigned int y = 0; y < ourPNG->height(); y++) {
       HSLAPixel & ourPixel = ourPNG->getPixel(x, y); 
-      HSLAPixel & newPixel = ourPNG->getPixel(ourPNG->width() - x - 1, ourPNG->height() - y - 1);
+      HSLAPixel & newPixel = newPNG.getPixel(ourPNG->width() - x - 1, ourPNG->height() - y - 1);
 
       newPixel = ourPixel; 
     }
   }
-  newPNG->writeToFile(outputFile);
+  newPNG.writeToFile(outputFile);
 }
+
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
+
 
   return png;
 }
