04a271a335b4fced2fbb75a837b24cb3a3585d88
Sarthak <pusarthak@gmail.com>
2021-02-16 10:04:00 -0600

    Fixed Scale()

--- mp_stickers/Image.cpp
+++ mp_stickers/Image.cpp
@@ -224,7 +224,12 @@ void Image::scale(double factor) {
 }
 
 void Image::scale(unsigned w, unsigned h) {
-    double scaleH = float(h) / width();
+    double scaleH = float(h) / height();
+    double scaleW = float(w) / width(); 
 //easier and faster to use existing scale() to perform preservation 
-    scale(scaleH);
+    if (scaleH < scaleW) {
+        scale(scaleH);
+    } else {
+        scale(scaleW);
+    }
 } 
