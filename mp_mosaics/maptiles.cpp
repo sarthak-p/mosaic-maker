0e8732fa04c7debd82796ec4292635492a54dfdb
Sarthak <pusarthak@gmail.com>
2021-03-27 17:50:45 -0500

    finished

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -10,7 +10,6 @@
 
 using namespace std;
 
-
 Point<3> convertToXYZ(LUVAPixel pixel) {
     return Point<3>( pixel.l, pixel.u, pixel.v );
 }
@@ -18,7 +17,23 @@ Point<3> convertToXYZ(LUVAPixel pixel) {
 MosaicCanvas* mapTiles(SourceImage const& theSource,
                        vector<TileImage>& theTiles)
 {
-    
-    return NULL;
-}
+    map<Point<3>, long> tiles;
+    vector<Point<3>> pixelPoints;
 
+    for (unsigned long i = 0; i < theTiles.size(); i++) {
+        Point<3> converted = convertToXYZ(theTiles[i].getAverageColor());
+        tiles[converted] = i;
+        pixelPoints.push_back(converted);
+    }
+
+    MosaicCanvas * output = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    KDTree<3> pointTree(pixelPoints); 
+
+    for (int r = 0; r < theSource.getRows(); r++) { 
+        for (int c = 0; c < theSource.getColumns(); c++) {
+            Point<3> converted1 = convertToXYZ(theSource.getRegionColor(r, c));
+            output->setTile(r, c, &theTiles[tiles[pointTree.findNearestNeighbor(converted1)]]);
+        }
+    }
+    return output; 
+}
